36165d73aed44233c22947794e01db341d27b6a9
AllenCodess <Aph296@protonmail.com>
2026-02-19 17:32:41 -0500

    added js to the navigation

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//toggle mobile menu
+
 document.addEventListener("DOMContentLoaded", function () {
   const toggleBtn = document.querySelector(".nav-container .mobile-menu-toggle");
   const mobileMenu = document.querySelector(".nav-container .mobile-menu-list-container");
@@ -5,4 +7,16 @@ document.addEventListener("DOMContentLoaded", function () {
   toggleBtn.addEventListener("click", function () {
     mobileMenu.classList.toggle("active");
   });
+});
+
+// Change navbar background on scroll
+
+const navbarBackground = document.querySelector(".nav-container");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 0) {
+    navbarBackground.classList.add("navbar-scroll");
+  } else {
+    navbarBackground.classList.remove("navbar-scroll");
+  }
 });
